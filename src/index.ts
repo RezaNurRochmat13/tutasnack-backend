4d3d66647d762ffaf69adda70d7ce281fe110c3d
RezaNurRochmat13 <rezanurrochmat@gmail.com>
2026-05-28 10:24:10 +0700

    feat(cors): enable cors

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono"
+import { cors } from "hono/cors"
 import type { Env } from "./config/env"
 import { registerRoutes } from "./routes"
 
 const app = new Hono<{ Bindings: Env }>()
+
+app.use("/*", cors())
 
 app.get("/", (c) => c.json({ message: "TutaSnack API", version: "1.0.0" }))
 
